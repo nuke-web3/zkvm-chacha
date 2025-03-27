0955e8daeaf2c9d47baf4d9f663fd555237b25e0
Nuke <nuke-web3@proton.me>
2025-03-27 16:26:29 -0600

    add realistic example input bin

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,2 @@
-pub const INPUT_BYTES_LENGTH: usize = 300_000;
+pub const INPUT_BYTES: &[u8] = include_bytes!("../../static/proof_input_example.bin");
+pub const INPUT_BYTES_LENGTH: usize = INPUT_BYTES.len();
