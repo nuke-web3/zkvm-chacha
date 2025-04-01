f65597be3bef2cc56610a16f846ebe32f3a0ee41
Nuke <nuke-web3@proton.me>
2025-04-01 10:04:09 -0600

    test data

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,2 +1,3 @@
 pub const INPUT_BYTES: &[u8] = include_bytes!("../../static/proof_input_example.bin");
+// pub const INPUT_BYTES: &[u8] = &[42;10_000];
 pub const INPUT_BYTES_LENGTH: usize = INPUT_BYTES.len();
